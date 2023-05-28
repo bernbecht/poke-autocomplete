3a528e94d3222e2ecb9d9c2c4f3dec3ffb07d571
Bernardo Bechtold <1577809+bernardoBechtold@users.noreply.github.com>
2023-05-28 10:48:50 +0200

    Adds debouncer when loading pokemons

--- src/data/useFetchPokemonSuggestions.ts
+++ src/data/useFetchPokemonSuggestions.ts
@@ -20,20 +20,25 @@ export function useFetchPokemonSuggestions() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [data, setData] = useState<Pokemon[]>([]);
   const [error, setError] = useState<Error>();
+  let filterTimeout: number;
 
   async function fetch(query: string) {
-    try {
-      const data = await fetchPokemons();
-      const pokemonSuggestions = data.results.filter((pokemon: Pokemon) => {
-        return pokemon.name
-          .toLocaleLowerCase()
-          .startsWith(query.toLocaleLowerCase());
-      });
-      setData(pokemonSuggestions);
-    } catch (error) {
-      setError(error as Error);
-    }
-    setIsLoading(false);
+    clearTimeout(filterTimeout);
+    setIsLoading(true);
+    filterTimeout = setTimeout(async () => {
+      try {
+        const data = await fetchPokemons();
+        const pokemonSuggestions = data.results.filter((pokemon: Pokemon) => {
+          return pokemon.name
+            .toLocaleLowerCase()
+            .startsWith(query.toLocaleLowerCase());
+        });
+        setData(pokemonSuggestions);
+      } catch (error) {
+        setError(error as Error);
+      }
+      setIsLoading(false);
+    }, 200);
   }
 
   return { isLoading, data, error, fetch };
